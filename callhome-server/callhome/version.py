132f2bd4230bd36ba7459648f9aac896f5cad268
Bartjan Hoogenbosch <bartjan.hoogenbosch@kpn.com>
2020-07-10 13:30:29 +0200

    Cleanup

--- callhome-server/callhome/version.py
+++
@@ -1,6 +0,0 @@
-import socket
-
-HOSTNAME = socket.gethostname()
-PROG_NAME = 'VPN CallHome Server'
-VERSION = '0.1'
-
--- callhome-server/config.py
+++
@@ -1,8 +0,0 @@
-# Certificates
-SERVER_CERT="/etc/dehydrated/certs/uxx-001.pc-mania.nl/cert.pem"
-SERVER_KEY="/etc/dehydrated/certs/uxx-001.pc-mania.nl/privkey.pem"
-
-# CallHome Port
-APP_PORT=8080
-APP_LISTENER="0.0.0.0"
-
